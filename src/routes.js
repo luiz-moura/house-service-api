5c0073454d0239c82a5e8a0b4602de30f9012966
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-08-28 08:40:05 -0400

    feat: created connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+// import Role from './app/models/Role';
+
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' })
-);
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Luiz Moura',
+    nickname: '🌀',
+    genre: 'senha',
+    status: 1,
+    email: 'luizhom@outlook.com',
+    password: '12345678',
+  });
+
+  response.json(user);
+});
 
 export default routes;
