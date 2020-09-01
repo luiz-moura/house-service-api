034ac0aab383da2e13e28cb1c45f43ec380e506a
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-09-01 11:51:45 -0400

    feat: user edition

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -25,9 +25,31 @@ class UserController {
   }
 
   async update(request, response) {
-    // console.log(request.userId);
+    const { email, oldPassword } = request.body;
 
-    return response.json({ ok: true });
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return response.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, genre, status } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+      genre,
+      status,
+    });
   }
 }
 
