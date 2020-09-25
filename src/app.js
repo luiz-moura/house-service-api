21951582062a7e302c838df9bc4e6c56b0e50534
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-09-25 09:45:50 -0400

    feat: sentry error tracking

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import path from 'path';
+import Youch from 'youch';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +33,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, request, response, next) => {
+      const errors = await new Youch(err, request).toJSON();
+
+      return response.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,5 @@
+export default {
+  dsn:
+    'https://a90491ac9e5d46f5bca3f67ed0e7d9ee@o450381.ingest.sentry.io/5441374',
+  tracesSampleRate: 1.0,
+};
