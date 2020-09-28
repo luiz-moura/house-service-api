170e8ebb765481fb95e1145d58260c30a8ea1dca
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-09-28 15:02:57 -0400

    chore: database models

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,13 @@ import Sequelize from 'sequelize';
 import Role from '../app/models/Role';
 import User from '../app/models/User';
 import File from '../app/models/File';
+import State from '../app/models/State';
+import City from '../app/models/City';
+import Address from '../app/models/Address';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Role, File];
+const models = [User, Role, File, State, City, Address];
 
 class Database {
   constructor() {
