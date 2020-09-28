ec93e4614d823fdf7ef29cff2b908665d317db45
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2020-09-28 15:02:12 -0400

    chore: routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,9 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import StateController from './app/controllers/StateController';
+import CityController from './app/controllers/CityController';
+import AddressController from './app/controllers/AddressController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,15 +21,30 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.get('/roles', RoleController.index);
-routes.post('/roles', RoleController.store);
-routes.put('/roles', RoleController.update);
-routes.delete('/roles', RoleController.delete);
-
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/roles', RoleController.index);
+routes.post('/roles', RoleController.store);
+routes.put('/roles/:id', RoleController.update);
+routes.delete('/roles/:id', RoleController.delete);
+
+routes.get('/states', StateController.index);
+routes.post('/states', StateController.store);
+routes.put('/states/:id', StateController.update);
+routes.delete('/states/:id', StateController.delete);
+
+routes.get('/cities', CityController.index);
+routes.post('/cities', CityController.store);
+routes.put('/cities/:id', CityController.update);
+routes.delete('/cities/:id', CityController.delete);
+
+routes.get('/adresses', AddressController.index);
+routes.post('/adresses', AddressController.store);
+routes.put('/adresses/:id', AddressController.update);
+routes.delete('/adresses/:id', AddressController.delete);
 
 export default routes;
